f5648cc6491eb7989c140deb9f22f1ed34d0c7c5
danielvtfs <danielvtfs@gmail.com>
2021-09-15 16:12:39 -0300

    add lombok e classe categoria

--- src/main/java/com/dfernandez/ecommercedist/EcommercedistApplication.java
+++ src/main/java/com/dfernandez/ecommercedist/EcommercedistApplication.java
@@ -1,13 +1,22 @@
 package com.dfernandez.ecommercedist;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.dfernandez.ecommercedist.domain.Categoria;
+
 @SpringBootApplication
-public class EcommercedistApplication {
+public class EcommercedistApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommercedistApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria c1 = new Categoria();
+		
 	}
 
 }
