943529ca8ac69ae02b899123d5348b493881c564
danielvtfs <danielvtfs@gmail.com>
2021-09-15 10:12:17 -0300

    criando o projeto

---
+++ src/main/java/com/dfernandez/ecommercedist/EcommercedistApplication.java
@@ -0,0 +1,13 @@
+package com.dfernandez.ecommercedist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercedistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercedistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dfernandez/ecommercedist/EcommercedistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dfernandez.ecommercedist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercedistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
