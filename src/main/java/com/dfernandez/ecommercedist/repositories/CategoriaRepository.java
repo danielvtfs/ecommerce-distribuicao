571c19033eec689389cb52518cef8bf7f6fb142c
danielvtfs <danielvtfs@gmail.com>
2021-09-16 09:49:12 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/dfernandez/ecommercedist/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.dfernandez.ecommercedist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dfernandez.ecommercedist.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+	
+
+}
--- src/main/java/com/dfernandez/ecommercedist/resources/CategoriaResource.java
+++ src/main/java/com/dfernandez/ecommercedist/resources/CategoriaResource.java
@@ -3,25 +3,31 @@ package com.dfernandez.ecommercedist.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dfernandez.ecommercedist.domain.Categoria;
+import com.dfernandez.ecommercedist.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(1, "Escritório");
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	
+	//@PathVariable para o parâmetro id receber o /{id}
+	//ResponseEntity encapsula as respostas http para o serviço rest
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria categoria = categoriaService.find(id);
 		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+		return ResponseEntity.ok().body(categoria);
+
 	}
 }
