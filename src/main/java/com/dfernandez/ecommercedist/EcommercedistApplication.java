0b838fe7907ebeb13cd9d0f6e03f0e5622a0497d
danielvtfs <danielvtfs@gmail.com>
2021-09-17 17:47:36 -0300

    corrigindo produto e associação muitos para muitos

--- src/main/java/com/dfernandez/ecommercedist/EcommercedistApplication.java
+++ src/main/java/com/dfernandez/ecommercedist/EcommercedistApplication.java
@@ -17,31 +17,32 @@ public class EcommercedistApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+	@Autowired
 	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommercedistApplication.class, args);
 	}
+	
 
 	@Override
 	public void run(String... args) throws Exception {
-		Categoria cat1 = new Categoria(null, "Informática");
-		Categoria cat2 = new Categoria(null, "escritório");
-		
-		Produto produto1 = new Produto(null, "Computador", 2000.00);
-		Produto produto2 = new Produto(null, "Impressora", 800.00);
-		Produto produto3 = new Produto(null, "Mouse", 80.00);
-		
-		cat1.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
-		cat2.getProdutos().addAll(Arrays.asList(produto2));
-		
-		produto1.getCategorias().addAll(Arrays.asList(cat1));
-		produto2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-		produto3.getCategorias().addAll(Arrays.asList(cat1));
-		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
-	}
 
-}
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+	}
+} 
