94bd2709373a1d250057800ab63c92f1bc8bea51
danielvtfs <danielvtfs@gmail.com>
2021-09-17 13:44:39 -0300

    Criando operações de instância

--- src/main/java/com/dfernandez/ecommercedist/EcommercedistApplication.java
+++ src/main/java/com/dfernandez/ecommercedist/EcommercedistApplication.java
@@ -1,13 +1,20 @@
 package com.dfernandez.ecommercedist;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.dfernandez.ecommercedist.domain.Categoria;
+import com.dfernandez.ecommercedist.repositories.CategoriaRepository;
 
 @SpringBootApplication
 public class EcommercedistApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommercedistApplication.class, args);
@@ -15,8 +22,10 @@ public class EcommercedistApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		Categoria c1 = new Categoria();
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "escritório");
 		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
