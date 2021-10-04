550e8a6a72b220b8cea1f8855d1ea7865fa4280e
danielvtfs <danielvtfs@gmail.com>
2021-10-04 07:21:18 -0300

    criando método POST para Categoria

--- src/main/java/com/dfernandez/ecommercedist/services/CategoriaService.java
+++ src/main/java/com/dfernandez/ecommercedist/services/CategoriaService.java
@@ -22,4 +22,9 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);//setId null para que o método save considere insert e não update
+		return repo.save(obj);
+	}
 }
